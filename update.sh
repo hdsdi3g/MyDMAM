5f6499ecc8b70614cb8a42722db72e8ee5dab7bc
hdsdi3g <admin@hd3g.tv>
2014-04-19 22:49:28 +0200

    Add licences and correct bad mirror version.

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-wget -m -k -p -c -E http://mydmam.org
+wget -m -k -p -c -E -e robots=off --exclude-directories=wp-admin,dwl,comment-page,trackback http://mydmam.org
+
+cp -rf mydmam.org/* .
+
+rm -rf mydmam.org
 
