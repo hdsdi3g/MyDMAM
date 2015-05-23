baf807d582b7d76c743044a226e30d265a0d0565
hdsdi3g <admin@hd3g.tv>
2015-05-23 18:28:39 +0200

    Update #111 mergue a-header and module js.

--- public/javascripts/src/a-header.js
+++ public/javascripts/src/a-header.js
@@ -17,7 +17,7 @@
 /*jshint eqnull:true, loopfunc:true, shadow:true, jquery:true */
 
 /**
- * Pre-definited strings functions. Init mydmam global object
+ * Pre-definited strings functions. Init mydmam global object.
  */
 (function(window) {
 	String.prototype.trim = function() {
@@ -68,4 +68,116 @@
 			 */
 		};
 	}
+	
+	mydmam.module = {};
+	
 })(window);
+
+/**
+ * Let Play/MyDMAM modules declare functions and callbacks.
+ * 
+ * register()
+ * @param module_name
+ * @param declarations: {functionToCallback: function, ...}
+ * @return null
+ * 
+ * dumpList()
+ * @return null
+ * 
+ * module.getCallbacks(callback_name)
+ * @return [{name: "Module name", callback: function}, ]
+ */
+(function(module) {
+	
+	/**
+	 * {functionToCallback: [{name: "Module name", callback: function}, ], ...}
+	 */
+	var modules = {};
+	
+	module.register = function(module_name, declarations) {
+		if (!module_name) {
+			throw "No module name for register !";
+		}
+		if (!declarations) {
+			throw "No correct declaration for register module: " + module_name;
+		}
+		for (var callback_name in declarations) {
+			if (!modules[callback_name]) {
+				modules[callback_name] = [];
+			}
+			modules[callback_name].push({
+				name: module_name,
+				callback: declarations[callback_name],
+			});
+		}
+	};
+	
+	module.dumpList = function() {
+		console.log(modules);
+	};
+	
+	module.getCallbacks = function(callback_name) {
+		if (modules[callback_name]) {
+			return modules[callback_name];
+		} else {
+			return [];
+		}
+	};
+
+	var multipleCallbacks = function(callback_name) {
+		return function(args) {
+			var callbacks = module.getCallbacks(callback_name);
+			var result;
+			var callback;
+			var module_name;
+			var module_response_callback = {};
+			
+			for (var pos in callbacks) {
+				callback = callbacks[pos];
+				module_name = callback.name;
+				try {
+					var result = callback.callback.apply(this, arguments);
+					module_response_callback[module_name] = result;
+				} catch (err) {
+					console.error(err, module_name, callback_name);
+				}
+			}
+			return module_response_callback;
+		};
+	};
+	
+	var firstValidCallback = function(callback_name) {
+		return function(args) {
+			var callbacks = module.getCallbacks(callback_name);
+			var result;
+			var callback;
+			
+			for (var pos in callbacks) {
+				callback = callbacks[pos];
+				try {
+					var result = callback.callback.apply(this, arguments);
+					if (result) {
+						return result;
+					}
+				} catch (err) {
+					console.error(err, callback.name, callback_name);
+				}
+			}
+			return null;
+		};
+	};
+
+	/**
+	 * Functions and callbacks declarations.
+	 * Call with module.f.MyFunction(param1, param2, ...).
+	 * @return {module_name: Result, ...} OR
+	 * @return FirstValidResponse
+	 */
+	
+	module.f = {};
+	module.f.helloworld = multipleCallbacks("helloworld");
+	module.f.processViewSearchResult = firstValidCallback("processViewSearchResult");
+	module.f.wantToHaveResolvedExternalPositions = firstValidCallback("wantToHaveResolvedExternalPositions");
+	module.f.i18nExternalPosition = firstValidCallback("i18nExternalPosition");
+	
+})(window.mydmam.module);
