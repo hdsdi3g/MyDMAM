2b966f0d7d4a2e654966fb5f664e6147bcf60ee6
hdsdi3g <admin@hd3g.tv>
2017-06-24 23:21:41 +0200

    Update #277 Polyvalent div resizer (mouse/touch)

--- app/react/mydmam/async/r-and-d.jsx
+++ app/react/mydmam/async/r-and-d.jsx
@@ -17,6 +17,183 @@
  */
 
 /**
+ * Polyvalent div resizer (mouse/touch).
+ * <async.ResizablePane mode="both">...items...</async.ResizablePane>
+ * With mode=both|width|height
+ */
+var ResizerHandle = createReactClass({
+	onMouseDown: function(e) {
+		this.props.onStartResize(e.clientX, e.clientY, this.props.behavior, false);
+	},
+	onTouchStart: function(e) {
+		e.preventDefault(),
+		this.props.onStartResize(e.changedTouches[0].clientX, e.changedTouches[0].clientY, this.props.behavior, true);
+	},
+	render: function() {
+		return (<div style={this.props.style} onMouseDown={this.onMouseDown} onTouchStart={this.onTouchStart}></div>);
+	}
+});
+
+async.ResizablePane = createReactClass({
+	getInitialState: function() {
+		return {
+			start_x: null,
+			start_y: null,
+			start_width: null,
+			start_height: null,
+			width: null,
+			height: null,
+			behavior: null,
+		};
+	},
+	onStartResize: function(x, y, behavior, is_touch) {
+		var resizeable = ReactDOM.findDOMNode(this.refs.resizeable);
+		var computed_style = document.defaultView.getComputedStyle(resizeable);
+
+		this.setState({
+			start_width: parseInt(computed_style.width, 10),
+			start_height: parseInt(computed_style.height, 10),
+			start_x: x,
+			start_y: y,
+			behavior: behavior,
+		}, function() {
+			if (is_touch) {
+				document.documentElement.addEventListener('touchmove', this.doTouchMove, false);
+				document.documentElement.addEventListener('touchend', this.doTouchEnd, false);
+			} else {
+				document.documentElement.addEventListener('mousemove', this.doDrag, false);
+				document.documentElement.addEventListener('mouseup', this.stopDrag, false);
+			}
+		}.bind(this));
+	},
+	doDrag: function(e) {
+		this.doResize(e.clientX, e.clientY);
+	},
+	doTouchMove: function(e) {
+		e.preventDefault();
+		this.doResize(e.changedTouches[0].clientX, e.changedTouches[0].clientY);
+	},
+	doResize: function(new_x, new_y) {
+		if (this.props.mode == "width") {
+			this.setState({
+				width: this.state.start_width + new_x - this.state.start_x,
+				height: this.state.start_height,
+			});
+		} else if (this.props.mode == "height") {
+			this.setState({
+				width: this.state.start_width,
+				height: this.state.start_height + new_y - this.state.start_y,
+			});
+		} else {
+			if (this.state.behavior == "width") {
+				this.setState({
+					width: this.state.start_width + new_x - this.state.start_x,
+					height: this.state.start_height,
+				});
+			} else if (this.state.behavior == "height") {
+				this.setState({
+					width: this.state.start_width,
+					height: this.state.start_height + new_y - this.state.start_y,
+				});
+			} else {
+				this.setState({
+					width: this.state.start_width + new_x - this.state.start_x,
+					height: this.state.start_height + new_y - this.state.start_y,
+				});
+			}
+		}
+	},
+	stopDrag: function() {
+	    document.documentElement.removeEventListener('mousemove', this.doDrag, false);
+	    document.documentElement.removeEventListener('mouseup', this.stopDrag, false);
+	},
+	doTouchEnd: function() {
+	    document.documentElement.removeEventListener('touchmove', this.doTouchMove, false);
+	    document.documentElement.removeEventListener('touchend', this.doTouchEnd, false);
+	},
+	componentWillUnmount: function() {
+		this.stopDrag();
+		this.doTouchEnd();
+	},
+	componentWillUpdate: function() {
+		window.getSelection().removeAllRanges();
+	},
+	render: function() {
+		var mode = this.props.mode;
+		if (mode == null) {
+			mode = "both";
+		}
+
+		var style = {
+			position: "relative",
+		};
+
+		if (this.state.width != null && this.state.height != null) {
+			style.width = this.state.width + "px";
+			style.height = this.state.height + "px";
+		}
+
+		var border_debug = null; //"1px solid #888";
+
+		var handles = [];
+		if (mode == "width") {
+			handles.push(<ResizerHandle style={{
+				width: "20px",
+				height: "100%",
+				position: "absolute",
+				border: border_debug,
+				right: 0,
+				top: 0,
+				cursor: "ew-resize",
+			}} key="w" onStartResize={this.onStartResize} behavior="width" />);
+		} else if (mode == "height") {
+			handles.push(<ResizerHandle style={{
+				width: "100%",
+				height: "20px",
+				position: "absolute",
+				border: border_debug,
+				left: 0,
+				bottom: 0,
+				cursor: "ns-resize",
+			}} key="h" onStartResize={this.onStartResize} behavior="height" />);
+		} else {
+			handles.push(<ResizerHandle style={{
+				width: "20px",
+				height: "100%",
+				position: "absolute",
+				border: border_debug,
+				right: 0,
+				top: 0,
+				cursor: "ew-resize",
+			}} key="w" onStartResize={this.onStartResize} behavior="width" />);
+			handles.push(<ResizerHandle style={{
+				width: "100%",
+				height: "20px",
+				position: "absolute",
+				border: border_debug,
+				left: 0,
+				bottom: 0,
+				cursor: "ns-resize",
+			}} key="h" onStartResize={this.onStartResize} behavior="height" />);
+			handles.push(<ResizerHandle style={{
+				width: "20px",
+				height: "20px",
+				position: "absolute",
+				border: border_debug,
+				right: 0,
+				bottom: 0,
+				cursor: "nwse-resize",
+			}} key="c" onStartResize={this.onStartResize} behavior="corner" />);
+		}
+
+		return (<div ref="resizeable" style={style}>
+			{this.props.children}
+			{handles}
+		</div>);
+	}
+});
+
+/**
  * getI18n() + formatBytes() Create correct file size i18n with unit selection.
  */
 var bytes_unit_sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
