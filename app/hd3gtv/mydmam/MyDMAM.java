1a64cd4bb6eab6313b9c3b01b47dff15c0f276b7
hdsdi3g <admin@hd3g.tv>
2017-07-21 00:46:49 +0200

    Update #280, add ISO Charset def, add Bridge SPIE

--- app/hd3gtv/mydmam/MyDMAM.java
+++ app/hd3gtv/mydmam/MyDMAM.java
@@ -55,6 +55,7 @@ public class MyDMAM {
 	public static String APP_COPYRIGHT = "Copyright (C) hdsdi3g for hd3g.tv 2012-2017";
 	
 	public static final Charset UTF8 = Charset.forName("UTF-8");
+	public static final Charset ISO_8859_1 = Charset.forName("ISO-8859-1");
 	
 	public static final Factory factory = new Factory();
 	
